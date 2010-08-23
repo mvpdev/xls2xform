97c84ab897252a25f9f2483a12828765b4392fd7
Andrew Marder <andrew.n.marder@gmail.com>
2010-08-23 13:02:42 +0000

    slight modification to database structure

--- models.py
+++ models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.core.files.storage import default_storage
+
+class File(models.Model):
+    file = models.FileField(upload_to="xls2xform/files/%Y%m%d%H%M%S", storage=default_storage) #")
+
+    def name(self):
+        return self.file.generate_filename()
 
 class Submission(models.Model):
-    file = models.FileField(upload_to="./submissions") #/%Y%m%d%H%M%S")
+    file = models.ForeignKey(File)
     error_msg = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.file.name()
 
 class XForm(models.Model):
     submission = models.ForeignKey(Submission)
-    text = models.TextField()
+    file = models.ForeignKey(File)
