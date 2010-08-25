6098e46e78675dbe0f5d366c0dce7c5b0584d62f
Alex Dorey <dorey415@gmail.com>
2010-08-25 19:18:06 +0000

    fixing spacing (texteditor's fault)

--- models.py
+++ models.py
@@ -5,13 +5,13 @@ file_options = { "upload_to" : "xls2xform/submissions/%Y%m%d%H%M%S",
                  "storage" : default_storage, }
 
 class Submission(models.Model):
-    file = models.FileField(**file_options)
-    error_msg = models.TextField()
-    private = models.BooleanField(default=True)
+	file = models.FileField(**file_options)
+	error_msg = models.TextField()
+	private = models.BooleanField(default=True)
 
 class XForm(models.Model):
-    submission = models.ForeignKey(Submission)
-    file = models.FileField(**file_options)
+	submission = models.ForeignKey(Submission)
+	file = models.FileField(**file_options)
 
 	def __unicode__(self):
 		return self.uri()
