d739804a9d6a962505d34ab0320e11da4007cefb
Alex Dorey <dorey415@gmail.com>
2010-08-26 00:07:57 -0400

    Includes instructions tab (could be fixed up)

--- views.py
+++ views.py
@@ -13,9 +13,10 @@ import os
 
 def convert_file(request):
     most_recent_survey = "surveys00.xls"
+    help_lines = open('xls2xform/xls2xform.py').read().split('"""')[1].split("\n") # A bit crude, but functional
     if request.method != "POST":
         # if nothing's posted give them an empty form
-        return render_to_response("upload.html", {"form": SubmissionForm(), "most_recent_survey": most_recent_survey})
+        return render_to_response("upload.html", {"form": SubmissionForm(), "most_recent_survey": most_recent_survey, "help_lines": help_lines})
     else:
         # otherwise pull the data out of the request and process it
         populated_form = SubmissionForm(request.POST, request.FILES)
@@ -27,16 +28,16 @@ def convert_file(request):
                 for survey in surveys:
                     x = XForm(submission=s, file=survey)
                     x.save()
-                    # list the files created
+                # list the files created
                 return render_to_response("list.html", {"list": XForm.objects.filter(submission=s)})
             except Exception, e:
                 # record and display any error messages
                 s.error_msg = e.__str__()
                 s.save()
-                return render_to_response("upload.html", {"msg": s.error_msg, "form": SubmissionForm(), "most_recent_survey": most_recent_survey})
+                return render_to_response("upload.html", {"msg": s.error_msg, "form": SubmissionForm(), "most_recent_survey": most_recent_survey, "help_string": help_string})
         else:
             # invalid forms should try uploading again
-            return render_to_response("upload.html", {"form": populated_form, "most_recent_survey": most_recent_survey})
+            return render_to_response("upload.html", {"form": populated_form, "most_recent_survey": most_recent_survey, "help_string": help_string})
 
 
 def download_xform(request, path):
