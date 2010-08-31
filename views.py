314fce64a4b1a89445f3903cb7bbd8475e33c56e
Andrew Marder <andrew.n.marder@gmail.com>
2010-08-31 21:53:20 +0000

    used dictionary to simplify multiple calls of render to response

--- views.py
+++ views.py
@@ -6,17 +6,28 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from forms import SubmissionForm
 from models import Submission, XForm
+import xls2xform
 from xls2xform import write_xforms, ConversionError
 from django.conf import settings
 import markdown
 import os
+import codecs, pydoc
 
 def convert_file(request):
-    most_recent_survey = "surveys00.xls"
-    documentation = markdown.markdown(write_xforms.__doc__)
+    kwargs = {"most_recent_survey" : "surveys-v0.1.xls"}
+
+    input_file = codecs.open("xls2xform/README.mkdn", mode="r", encoding="utf8")
+    text = input_file.read()
+    kwargs["documentation"] = markdown.markdown(text)
+
+    textdoc = pydoc.TextDoc()
+    kwargs["api"] = pydoc.plain( textdoc.docmodule(xls2xform) )
+
+    kwargs["form"] = SubmissionForm()
+
     if request.method != "POST":
         # if nothing's posted give them an empty form
-        return render_to_response("upload.html", {"form": SubmissionForm(), "most_recent_survey": most_recent_survey, "documentation": documentation})
+        return render_to_response("upload.html", kwargs)
     else:
         # otherwise pull the data out of the request and process it
         populated_form = SubmissionForm(request.POST, request.FILES)
@@ -34,10 +45,12 @@ def convert_file(request):
                 # record and display any error messages
                 s.error_msg = e.__str__()
                 s.save()
-                return render_to_response("upload.html", {"msg": s.error_msg, "form": SubmissionForm(), "most_recent_survey": most_recent_survey, "documentation": documentation})
+                kwargs["msg"] = s.error_msg
+                return render_to_response("upload.html", kwargs)
         else:
             # invalid forms should try uploading again
-            return render_to_response("upload.html", {"form": populated_form, "most_recent_survey": most_recent_survey, "documentation": documentation})
+            kwargs["form"] = populated_form
+            return render_to_response("upload.html", kwargs)
 
 
 def download_xform(request, path):
