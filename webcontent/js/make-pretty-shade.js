5efeed3a6d35adafe198c4041ff64e686ec377db
Alex Dorey <dorey415@gmail.com>
2011-01-22 23:38:23 -0500

    Restoring old button and adding xls2xformer to javascript effects file (loaded after the important stuff of the page has loaded.)

--- webcontent/js/make-pretty-shade.js
+++ webcontent/js/make-pretty-shade.js
@@ -1,3 +1,36 @@
+var xformer = (function(options){
+	var img, div, defaultOpts, opts;
+
+	defaultOpts = {
+		'opacity': 0.7,
+		pulseTime: 750
+	}
+	opts = $.extend(defaultOpts, options);
+
+	div = $("<div />", {'style': 'position:fixed;right:75px;width:114px;height:127px;top:7px;z-index:-1'});
+	div.append(img);
+	function loadXformer() {
+		img = $("<img />");
+		img.css({'opacity':0});
+		img.load(function(){
+		  div.appendTo('body');
+		  $(this).animate({'opacity':opts.opacity});
+		});
+		img.attr('src','/sm/images/xls2xformer.png');
+	}
+	return {
+	  load: loadXformer,
+	  pulse: function(cb){
+	    if('function'!==typeof cb) {
+	      var cb = function(){}
+        }
+	    img.animate({'opacity':1}, opts.pulseTime, function(){
+	      img.animate({'opacity': opts.opacity}, opts.pulseTime, cb)
+	    })
+	  }
+	}
+})({opacity:0.69});
+
 var PrettyShade = (function(){
 	var bgSrc	=	"/sm/images/shading/bg-shade.png";
 	function appearImage() {
@@ -7,6 +40,9 @@ var PrettyShade = (function(){
 	return appearImage;
 })();
 
-if($(window).width()>800) {
-	PrettyShade();
-}
+$(function(){
+    xformer.load();
+    if($(window).width()>800) {
+    	PrettyShade();
+    }
+});
