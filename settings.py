d46531f74bdc49bab954c3b384edaadb18cada79
Alex Dorey <dorey415@gmail.com>
2011-06-13 13:11:29 -0400

    adding django-registration to requirements and settings.

--- settings.py
+++ settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'south',
     'xls2xform.main',
+    'registration',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
