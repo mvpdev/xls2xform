9abb3103d9604cc95f00a5312ba133c8704b923b
Andrew Marder <andrew.ei.marder@gmail.com>
2011-06-13 07:53:29 -0700

    Moved templates into app directory.

--- settings.py
+++ settings.py
@@ -117,7 +117,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'south',
-    'main',
+    'xls2xform.main',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
