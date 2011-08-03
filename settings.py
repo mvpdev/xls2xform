8a2fe30aaeec5c9302d16eafa2f84b27c90f6fe6
Alex Dorey <dorey415@gmail.com>
2011-08-03 13:50:54 -0400

    setting templates dir relative to CURRENT_DIR for production

--- settings.py
+++ settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'xls2xform.urls'
 
 TEMPLATE_DIRS = (
-    'templates',
+    os.path.join(CURRENT_DIR, 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
