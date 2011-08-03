44282b346107d94cd9450c6db7f080dd45a9cacd
Andrew Marder <andrew.ei.marder@gmail.com>
2011-08-03 09:17:03 -0700

    Cleaned up urls a bit.

--- xform_builder/urls.py
+++ xform_builder/urls.py
@@ -1,18 +1,14 @@
-from django.conf.urls.defaults import *
-from django.core.urlresolvers import reverse
+from django.conf.urls.defaults import patterns, url
 
 from xform_builder import views as main_views
 
+# todo: improve the regular expression \S+
+EDIT_XFORM = r"^edit/(?P<survey_id>\S+)/"
+
 urlpatterns = patterns('',
-    url(r"^quick_converter/$", main_views.quick_converter),
-    url(r"^$", main_views.index),
-    url(r"^edit/(?P<survey_id>\S+)/download/$", main_views.download_xform),
-    url(r"^edit/(?P<survey_id>\S+)/download/(?P<version_number>\S+)/(?P<xform_file_name>\S+)\.xml", main_views.download_xform),
-    url(r"^edit/(?P<survey_id>\S+)/section/(?P<section_slug>\S+)/(?P<action>\S+)", main_views.edit_section),
-#    url(r"^edit/(?P<survey_id>\S+)/validate", main_views.validate_xform),
-    url(r"^edit/(?P<survey_id>\S+)/debug_json", main_views.debug_json),
-    url(r"^edit/(?P<survey_id>\S+)/", main_views.edit_xform),
+    url(r"^$", main_views.home),
+    url(EDIT_XFORM + r"download/$", main_views.download_xform),
+    url(EDIT_XFORM + r"section/(?P<section_slug>\S+)/(?P<action>\S+)", main_views.edit_section),
+    url(EDIT_XFORM + r"debug_json", main_views.debug_json),
+    url(EDIT_XFORM + r"", main_views.edit_xform),
 )
-
-
-#(?P<pk>\d+)
