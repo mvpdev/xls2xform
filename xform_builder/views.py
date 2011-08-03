095237d32cc953b3192bd63bc5854f522d01e878
Andrew Marder <andrew.ei.marder@gmail.com>
2011-08-03 09:28:31 -0700

    Got quick converter working with original xls2xform and pyxform. Cleaned up homepage so if the user is not logged in he sees the quick converter, if he is logged in he sees the list of his xforms. Cleaned up the download view to return a named file without relying on the url.

--- xform_builder/views.py
+++ xform_builder/views.py
@@ -11,7 +11,9 @@ from django.template.defaultfilters import slugify as django_slugify
 
 from xform_builder.models import XForm
 from pyxform.xls2json import SurveyReader
+from pyxform.builder import create_survey_from_path
 from xls2xform import settings
+from original_xls2xform.xls2xform import write_xforms
 
 
 def slugify(str):
@@ -22,32 +24,80 @@ class QuickConverter(forms.Form):
     xls_file = forms.FileField(label="XLS File")
 
     def _get_xforms_using_original_xls2xform(self):
-        # to use the original xls2xform script we have to save the
-        # xls_file to disk and call write_xforms with the path to the
-        # xls file.
-        pass
+        """
+        Try using the original xls2xform script. If there is an
+        exception return the exception, otherwise return a dictionary
+        where the keys are the filenames and the values are the xml of
+        the corresponding xform. todo: we need to delete the files
+        created by write_xforms.
+        """
+        try:
+            xforms = write_xforms(self.path)
+        except Exception as e:
+            return e
+        result = {}
+        for xform in xforms:
+            with open(xform, "r") as f:
+                file_name = os.path.basename(xform)
+                result[file_name] = f.read()
+        return result
 
     def _get_xform_using_pyxform(self):
-        assert False, self.xls_file
+        """
+        Try using pyxform to convert the xls file into an xform. If
+        there is an exception return the exception, otherwise return a
+        dictionary where the single key is the desired file name and
+        the value is the xml of the xform.
+        """
+        try:
+            survey = create_survey_from_path(self.path)
+            xform_str = survey.to_xml()
+        except Exception as e:
+            return e
+        file_name = survey.id_string() + ".xml"
+        return {file_name: xform_str}
 
     def get_xforms(self):
-        # If the pyxform conversion works, return the corresponding
-        # xform. If the original xls2xform conversion works return
-        # the corresponding xforms. Otherwise return a pair of the two
-        # exceptions that were thrown.
-        return self._get_xform_using_pyxform()
+        """
+        Try using both the old and new conversion methods on the
+        passed in xls file. If the new method works return the
+        resulting xform. If the old method works return the resulting
+        xform. Otherwise throw the exception from the new method. It
+        would be better to give the exception from both methods.
+        """
+        xls = self.cleaned_data['xls_file']
+        self.path = save_in_temp_dir(xls)
+
+        old = self._get_xforms_using_original_xls2xform()
+        new = self._get_xform_using_pyxform()
+        os.remove(self.path)
+
+        if type(new) is dict:
+            return self._package(new)
+        elif type(old) is dict:
+            return self._package(old)
+        else:
+            # it would be good to figure out which error message to
+            # send back
+            raise new
+
+    def _package(self, d):
+        assert len(d) == 1, "Code only supports single XForms."
+        filename = d.keys()[0]
+        response = HttpResponse(d[filename], mimetype="application/download")
+        response['Content-Disposition'] = 'attachment; filename=%s' % filename
+        return response
 
 
 def quick_converter(request):
-    context = RequestContext(request)
-    context.form = QuickConverter()
-
     if request.method == 'POST':
-        context.form = QuickConverter(request.POST)
-        if context.form.is_valid():
-            return context.form.get_xforms()
-        assert False
-
+        form = QuickConverter(request.POST, request.FILES)
+        if form.is_valid():
+            return form.get_xforms()
+    else:
+        form = QuickConverter()
+    context = RequestContext(request)
+    context.form = form
     return render_to_response(
         'quick_converter.html', context_instance=context
         )
@@ -65,6 +115,13 @@ class CreateXForm(forms.Form):
         if existing_forms > 0:
             raise forms.ValidationError("You already have a form with this ID string: %s" % id_string)
         return id_string
+
+
+def home(request):
+    if request.user.is_authenticated():
+        return index(request)
+    else:
+        return quick_converter(request)
 
 
 @login_required
@@ -94,19 +151,15 @@ def index(request):
     return render_to_response("index.html", context_instance=context)
 
 
-def download_xform(request, survey_id, version_number=None, xform_file_name=None):
+def download_xform(request, survey_id):
     xforms = request.user.xforms
     xform = xforms.get(id_string=survey_id)
-    if version_number is None:
-        version_number = xform.latest_version.version_number
-        survey_object = xform.export_survey()
-        xf_filename = "%s.xml" % survey_object.id_string()
-        return HttpResponseRedirect("/edit/%s/download/%s/%s" % (survey_id, version_number, xf_filename))
-    else:
-        survey_object = xform.export_survey()
-        xf_filename = "%s.xml" % survey_object.id_string()
-        xform_str = survey_object.to_xml()
-        return HttpResponse(xform_str, mimetype="application/download")
+    survey_object = xform.export_survey()
+    xf_filename = "%s.xml" % survey_object.id_string()
+    xform_str = survey_object.to_xml()
+    response = HttpResponse(xform_str, mimetype="application/download")
+    response['Content-Disposition'] = 'attachment; filename=%s' % xf_filename
+    return response
 
 
 def convert_file_to_json(file_io):
@@ -121,24 +174,29 @@ def convert_file_to_json(file_io):
     return (slug, section_json)
 
 
+def save_in_temp_dir(file_io):
+    file_name = file_io.name
+    tmp_xls_dir = os.path.join(settings.CURRENT_DIR, "xls_tmp")
+    if not os.path.exists(tmp_xls_dir):
+        os.mkdir(tmp_xls_dir)
+    path = os.path.join(tmp_xls_dir, file_name)
+    f = open(path, 'w')
+    f.write(file_io.read())
+    f.close()
+    return path
+
+
 def process_xls_io_to_section_json(file_io):
     # I agree that this function is not pretty, but I don't think we
     # should move this into the model because I prefer to think of the
     # model as file-format independent.
-    file_name = file_io.name
-    slug = re.sub("\.xlsx?", "", file_name)
-    tmp_file_name = file_name
-    tmp_xls_dir = os.path.join(settings.CURRENT_DIR, "xls_tmp")
-    if not os.path.exists(tmp_xls_dir):
-        os.mkdir(tmp_xls_dir)
-    tmp_xls_file = os.path.join(tmp_xls_dir, tmp_file_name)
-    f = open(tmp_xls_file, 'w')
-    f.write(file_io.read())
-    f.close()
-    xlr = SurveyReader(tmp_xls_file)
+    path = save_in_temp_dir(file_io)
+    m = re.search(r'([^/]+).xls$', path)
+    slug = m.group(1)
+    xlr = SurveyReader(path)
     xls_vals = xlr.to_dict()
     qjson = json.dumps(xls_vals)
-    os.remove(tmp_xls_file)
+    os.remove(path)
     return (slug, qjson)
 
 
